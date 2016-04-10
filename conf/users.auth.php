539417a39bba4a1fe6688138d630f783b671d286
gricha2380 <gregor.com@mac.com>
2016-04-10 15:57:09 -0700

    First push

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+picard:$1$015jUZn0$qTpCFPPV6pOeELba7A5Fa1:gricha2380:gricha2380@gmail.com:admin,user
